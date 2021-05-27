eed06bbf79bb9a91459e0b5dc1ba38a4d3ec5aa1
jbear18 <theyodelingcowgirl24@gmail.com>
2021-05-26 19:59:20 -0700

    OlympicRings, AdvancedRobotRace finished

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,110 @@
 package _01_Olympic_Rings;
+import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot one = new Robot(500,300, "mini");
+		Robot two = new Robot(600,300, "mini");
+		Robot three = new Robot(700,300, "mini");
+		Robot four = new Robot(550, 375, "mini");
+		Robot five = new Robot(650, 375, "mini");
+		
+		
+		Thread r1 = new Thread(()->{
+			one.setSpeed(20);
+			one.setPenColor(Color.BLUE);
+			one.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				one.penUp();
+				one.turn(4);
+				one.penDown();
+				one.move(4);
+				
+				counter ++;
+			}
+			});
+		
+		Thread r2 = new Thread(()->{
+			two.setSpeed(20);
+			two.setPenColor(Color.BLACK);
+			two.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				two.penUp();
+				two.turn(4);
+				two.penDown();
+				two.move(4);
+				
+				counter ++;
+			}
+		});
+		
+		
+		
+		Thread r3 = new Thread(()->{
+			three.setSpeed(20);
+			three.setPenColor(Color.RED);
+			three.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				three.penUp();
+				three.turn(4);
+				three.penDown();
+				three.move(4);
+				
+				counter ++;
+			}
+		});
+		
+		Thread r4 = new Thread(()->{
+			four.setSpeed(20);
+			four.setPenColor(Color.YELLOW);
+			four.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				four.penUp();
+				four.turn(4);
+				four.penDown();
+				four.move(4);
+				
+				counter ++;
+			}
+		});
+		
+		
+		Thread r5 = new Thread(()->{
+			five.setSpeed(20);
+			five.setPenColor(Color.GREEN);
+			five.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				five.penUp();
+				five.turn(4);
+				five.penDown();
+				five.move(4);
+				
+				counter ++;
+			}
+		});
+		
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
 
+		
+
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,133 @@
 package _02_Advanced_Robot_Race;
 
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	static // Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+
+	boolean isRacing = true;
+	
+	
+	public static void main(String[] args) {
+		Robot one = new Robot(300,300);
+		Robot two = new Robot(300,300, "mini");
+		Robot three = new Robot(300,300, "batman");
+		Robot four = new Robot(300, 300);
+		Robot five = new Robot(300, 300, "mini");
+
+		Thread r1 = new Thread(()->{
+			one.setPenColor(Color.BLUE);
+			one.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				int speed = (int) (Math.random() * 20) + 1;
+				one.setSpeed(speed);
+				one.penUp();
+				one.turn(4);
+//				one.penDown();
+				one.move(20);
+				
+				counter ++;
+			
+	
+				if(counter == 90) {
+					isRacing = false;
+				}
+			}
+			});
+		
+		Thread r2 = new Thread(()->{
+			two.setPenColor(Color.BLACK);
+			two.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				int speed = (int) (Math.random() * 20) + 1;
+				two.setSpeed(speed);
+				two.penUp();
+				two.turn(4);
+//				two.penDown();
+				two.move(20);
+				
+				counter ++;
+			}
+		});
+		
+		
+		
+		Thread r3 = new Thread(()->{
+			three.setPenColor(Color.RED);
+			three.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				int speed = (int) (Math.random() * 20) + 1;
+				three.setSpeed(speed);
+				three.penUp();
+				three.turn(4);
+//				three.penDown();
+				three.move(20);
+				
+				counter ++;
+			}
+		});
+		
+		Thread r4 = new Thread(()->{
+
+			four.setPenColor(Color.YELLOW);
+			four.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				int speed = (int) (Math.random() * 20) + 1;
+				four.setSpeed(speed);
+				four.penUp();
+				four.turn(4);
+//				four.penDown();
+				four.move(20);
+				
+				counter ++;
+			}
+		});
+		
+		
+		Thread r5 = new Thread(()->{
+			five.setPenColor(Color.MAGENTA);
+			five.penUp();
+			
+			int counter = 0;
+			while(counter <= 90) {
+				int speed = (int) (Math.random() * 20) + 1;
+				five.setSpeed(speed);
+				five.penUp();
+				five.turn(4);
+//				five.penDown();
+				five.move(20);
+				
+				counter ++;
+			}
+		});
+		
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+
+		
+		
+		
+		
+
+	}
+	
+	
 }
